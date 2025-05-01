080e62805e7de9d248d0462d311a759f56899240
luis-fbs <luis-fbs@outlook.com>
2025-04-30 21:02:59 -0300

    Change database schema

--- sqlite/db.py
+++ sqlite/db.py
@@ -5,7 +5,6 @@ db = "rbcml.db"
 
 def create_user(user):
     with sqlite3.connect(db) as connection:
-        connection.execute("PRAGMA foreign_keys = ON")
         cursor = connection.cursor()
         try:
             query = f"""INSERT INTO User 
@@ -18,39 +17,13 @@ def create_user(user):
 
 def create_model(model):
     with sqlite3.connect(db) as connection:
-        connection.execute("PRAGMA foreign_keys = ON")
         cursor = connection.cursor()
 
         try:
-            query = f"""INSERT INTO MODEL (Name, Description) 
-                            VALUES ("{model['name']}", "{model['description']}")"""
+            query = f"""INSERT INTO Model {tuple(model.keys())}  
+                            VALUES {tuple(model.values())}"""
             cursor.execute(query)
-            model_id = cursor.lastrowid
-
-            for role in model['roles']:
-                query = f"""INSERT OR IGNORE INTO Capability (SVideo, Rvideo, SAudio, RAudio, SString, RString) 
-                                VALUES {role['capabilities']}"""
-                cursor.execute(query)
-                if cursor.rowcount == 0:
-                               capabilities = role['capabilities']
-                               query = f"""SELECT Id FROM Capability WHERE SVideo = {capabilities[0]} AND 
-                                                                         RVideo = {capabilities[1]} AND 
-                                                                         SAudio = {capabilities[2]} AND 
-                                                                         RAudio = {capabilities[3]} AND 
-                                                                         SString = {capabilities[4]} AND 
-                                                                         RString = {capabilities[5]}"""
-                               cursor.execute(query)
-                               capability_id = cursor.fetchone()[0]
-                               query = f"""INSERT INTO Role (Name, ModelId, CapabilityId) 
-                                                VALUES ("{role['name']}", {model_id}, {capability_id})"""
-                               cursor.execute(query)
-                else:
-                    capability_id = cursor.lastrowid
-                    query = f"""INSERT INTO Role (Name, ModelId, CapabilityId)
-                                    VALUES ("{role['name']}", {model_id}, {capability_id})"""
-                    cursor.execute(query)
-
-            return "Model created successfully!"
+            return "Model-{cursor.lastrowid} created successfully!"
                   
         except Exception as e:
             return f"Error: {e}"
@@ -60,17 +33,11 @@ def create_session(session):
         connection.execute("PRAGMA foreign_key = ON")
         cursor = connection.cursor() 
         try:
-            participants = session['participants']
-            del session['participants']
             query = f"""INSERT INTO Session {tuple(session.keys())}
                             VALUES {tuple(session.values())}"""
             cursor.execute(query)
-            session_id = cursor.lastrowid
-            for participant in participants:
-                query = f"""INSERT INTO Participant
-                                VALUES {tuple(participant.values()) + (session_id,)}"""
-                cursor.execute(query)
-            return "Session created successfully!"
+            return "Session-{cursor.lastrowid} created successfully!"
         
         except Exception as e:
             return f"Error: {e}"
+
