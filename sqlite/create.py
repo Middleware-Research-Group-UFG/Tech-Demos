c9d199bebea76f1540c752ca525316267b399e41
luis-fbs <luis-fbs@outlook.com>
2025-04-30 22:35:20 -0300

    Based on the new shema, create user, model and session example

--- sqlite/create.py
+++ sqlite/create.py
@@ -2,60 +2,61 @@ import db
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
-user1 = {
-    "tag": "cyber_bob",
-    "name": "Bob",
-    "email": "rbcmlproject@gmail.com",
-    "password": "hello_world"
+user_alice = {
+    "Tag": "cyber_alice",
+    "Name": "Alice",
+    "Email": "rbcmlproject@gmail.com",
+    "Password": "HeLl0_w0rLd!"
 }
 
-user2 = {
-    "tag": "cyber_alice",
-    "name": "Alice",
-    "email": "rbcmlproject@gmail.com",
-    "password": "HeLl0_w0rLd!"
-}
-
-role1 = {
-    "name": "Principal",
-    "capabilities": (True, True, True, True, True, True)
-}
-
-role2 = {
-    "name": "Professor",
-    "capabilities": (True, True, True, True, True, True)
-}
-
-role3 = {
-    "name": "Student",
-    "capabilities": (False, True, False, True, True, True)
+user_bob = {
+    "Tag": "cyber_bob",
+    "Name": "Bob",
+    "Email": "rbcmlproject@gmail.com",
+    "Password": "hello_world"
 }
 
 model1 = {
-    "name": "School news",
-    "description": "This model can be used by schools to announce news",
-    "roles": [role1, role2, role3]
-}
-
-participant1 = {
-    "role": 1,
-    "userTag": "cyber_alice" 
-}
-
-participant2 = {
-    "role": 3,
-    "userTag": "cyber_bob"
+    "Name": "School news",
+    "Description": "This model can be used by schools to announce news",
+    "Definition": """{
+                        "roles": ["Student", "Professor", "Principal"],
+                        "connections":[
+                            {
+                                "Principal-Professor-Student": [
+                                    [true, true, true, true, true, true, true, false],
+                                    [false, true, true, true, true, true, true, false],
+                                    [false, true, false, true, true, true, false, true]
+                                ]
+                            },
+                            {   
+                                "Principal-Professor": [
+                                    [true, true, true, true, true, true, true, false],
+                                    [true, true, true, true, true, true, false, true]
+                                ]
+                            },
+                            {
+                                "Professor-Student": [
+                                    [true, true, true, true, true, true, true, false],
+                                    [false, true, true, true, true, true, false, true]
+                                ]
+                            }
+                        ]
+                    }"""
 }
 
 session1 = {
-    "creator": "cyber_alice",
-    "modelId": 1,
-    "expirationDate": str(datetime.now(ZoneInfo("UTC")) + timedelta(days=3))[0:19],
-    "participants": [participant1, participant2]
+    "Creator": "cyber_alice",
+    "ModelId": 1,
+    "ExpirationDate": str(datetime.now(ZoneInfo("UTC")) + timedelta(days=3))[0:19],
+    "Participants": """{ 
+                          "cyber_alice": ["Principal"],  
+                          "cyber_bob": ["Professor", "Student"]
+                        }"""
 }
 
-print(db.create_user(user1))
-print(db.create_user(user2))
+print(db.create_user(user_alice))
+print(db.create_user(user_bob))
 print(db.create_model(model1))
 print(db.create_session(session1))
 
